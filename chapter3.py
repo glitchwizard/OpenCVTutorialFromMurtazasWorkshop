04baceab1bd9f5ad6b1b3ae371d88e7d1fd95987
charley mcgowan <charley.mcgowan@outlook.com>
2020-12-25 14:39:44 -0800

    Img resize code complete

---
+++ chapter3.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("Resources/lambo.webp")
+print(img.shape)
+
+imgResize = cv2.resize(img, (300,200))
+
+cv2.imshow("Image",img)
+cv2.imshow("Image Resize", imgResize)
+
+cv2.waitKey(0)
