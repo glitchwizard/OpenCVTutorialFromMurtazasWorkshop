93d094c59f4cdb4e72fcbe976dc395f3eb85cd35
charley mcgowan <charley.mcgowan@outlook.com>
2020-12-25 14:46:25 -0800

    Cropped image commands added

--- chapter3.py
+++ chapter3.py
@@ -6,7 +6,11 @@ print(img.shape)
 
 imgResize = cv2.resize(img, (300,200))
 
+imgCropped = img[600:1000, 500:900]
+
+
 cv2.imshow("Image",img)
 cv2.imshow("Image Resize", imgResize)
+cv2.imshow("Image Cropped", imgCropped)
 
 cv2.waitKey(0)
