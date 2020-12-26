61ded4bfe614946e84a9230cc5979c1850f6639a
charley mcgowan <charley.mcgowan@outlook.com>
2020-12-25 18:13:25 -0800

    added code for a circle

--- chapter4.py
+++ chapter4.py
@@ -7,7 +7,8 @@ img = np.zeros((512, 512, 3), np.uint8)
 #img[200:300, 100:300] = 255, 0, 0
 
 cv2.line(img, (0, 0), (img.shape[1],img.shape[0]), (0, 255, 0),1)
-cv2.rectangle(img, (0, 0), (250, 350), (0, 0, 255), )
+cv2.rectangle(img, (0, 0), (250, 350), (0, 0, 255), 1)
+cv2.circle(img, (400, 50), 30, (255, 255, 0), 2)
 
 cv2.imshow("Image", img)
 
