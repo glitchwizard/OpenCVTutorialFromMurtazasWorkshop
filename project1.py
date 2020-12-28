871f027445410c13e59211a8b60d2b165f6329ab
charley mcgowan <charley.mcgowan@outlook.com>
2020-12-27 20:54:30 -0800

    Colors are showing up on the screen, with tip indication

--- project1.py
+++ project1.py
@@ -23,12 +23,28 @@ def findColor(img, myColors):
         upper = np.array(color[3:6])
 
         mask = cv2.inRange(imgHSV, lower, upper)
-        cv2.imshow(str(color[6]), mask)
+        x, y = getContours(mask)
+        cv2.circle(imgResult,(x,y), 10, (255,0,0), cv2.FILLED)
+        #cv2.imshow(str(color[6]), mask)
+
+def getContours(img):
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    x,y,w,h = 0, 0, 0, 0
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        if area > 500:
+            cv2.drawContours(imgResult, contour, -1, [255,0,255], 3)
+            perimeter = cv2.arcLength(contour, True)
+            approximate_contour = cv2.approxPolyDP(contour, 0.02*perimeter, True)
+            x, y, w, h = cv2.boundingRect(approximate_contour)
+
+    return x+w//2, y
 
 while True:
     success, img = cap.read()
+    imgResult = img.copy()
     findColor(img, myColors)
-    cv2.imshow("Video", img)
+    cv2.imshow("Video", imgResult)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
