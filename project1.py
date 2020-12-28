478a65a9a747ba4bd6ca73ee17ebcef308c73ff7
charley mcgowan <charley.mcgowan@outlook.com>
2020-12-28 12:20:40 -0800

    completed the project

--- project1.py
+++ project1.py
@@ -17,10 +17,14 @@ myColorValues = [                   ##this is BGR, not RGB
                     [0, 255, 0]     # Green
                 ]
 
+    #this is a list of points for drawing on canvas
+myPoints = [] #[ x, y, colorId]
 
-def findColor(img, myColors):
+
+def findColor(img, myColors, myColorValues):
     imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     count = 0
+    newPoints = []
     for color in myColors:
         # lower = np.array([h_min, s_min, v_min])
         # upper = np.array([h_max, s_max, v_max])
@@ -31,8 +35,12 @@ def findColor(img, myColors):
         mask = cv2.inRange(imgHSV, lower, upper)
         x, y = getContours(mask)
         cv2.circle(imgResult,(x,y), 10, myColorValues[count], cv2.FILLED)
+        if x != 0 and y != 0:
+            newPoints.append([x,y,count])
         count += 1
         #cv2.imshow(str(color[6]), mask)
+    return newPoints
+
 
 def getContours(img):
     contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
@@ -40,17 +48,30 @@ def getContours(img):
     for contour in contours:
         area = cv2.contourArea(contour)
         if area > 500:
-            cv2.drawContours(imgResult, contour, -1, [255,0,255], 3)
+            #cv2.drawContours(imgResult, contour, -1, [255,0,255], 3)
             perimeter = cv2.arcLength(contour, True)
             approximate_contour = cv2.approxPolyDP(contour, 0.02*perimeter, True)
             x, y, w, h = cv2.boundingRect(approximate_contour)
 
     return x+w//2, y
 
+def drawOnCanvas(myPoints,myColorValues):
+    for point in myPoints:
+        cv2.circle(imgResult, (point[0], point[1]), 10, myColorValues[point[2]], cv2.FILLED)
+
+
 while True:
     success, img = cap.read()
     imgResult = img.copy()
-    findColor(img, myColors)
+    newPoints = findColor(img, myColors, myColorValues)
+
+    if len(newPoints) != 0:
+        for newPoint in newPoints:
+            myPoints.append(newPoint)
+
+    if len(myPoints) != 0:
+        drawOnCanvas(myPoints,myColorValues)
+
     cv2.imshow("Video", imgResult)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
