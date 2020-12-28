283620b559030324ad4c9f6614d6b0d71644e8be
charley mcgowan <charley.mcgowan@outlook.com>
2020-12-27 19:07:53 -0800

    Values set to get a good looking mask

--- chapter7.py
+++ chapter7.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 def empty(a):
     pass
@@ -6,11 +7,11 @@ def empty(a):
 path = 'Resources\lambo.webp'
 cv2.namedWindow("TrackBars")
 cv2.resizeWindow("TrackBars", 640, 240)
-cv2.createTrackbar("Hue Min", "TrackBars", 0, 179, empty)
-cv2.createTrackbar("Hue Max", "TrackBars", 179, 179, empty)
-cv2.createTrackbar("Sat Min", "TrackBars", 0, 255, empty)
+cv2.createTrackbar("Hue Min", "TrackBars", 42, 179, empty)
+cv2.createTrackbar("Hue Max", "TrackBars", 74, 179, empty)
+cv2.createTrackbar("Sat Min", "TrackBars", 117, 255, empty)
 cv2.createTrackbar("Sat Max", "TrackBars", 255, 255, empty)
-cv2.createTrackbar("Val Min", "TrackBars", 0, 255, empty)
+cv2.createTrackbar("Val Min", "TrackBars", 49, 255, empty)
 cv2.createTrackbar("Val Max", "TrackBars", 255, 255, empty)
 
 while True:
@@ -23,9 +24,11 @@ while True:
     s_max = cv2.getTrackbarPos("Sat Max","TrackBars")
     v_min = cv2.getTrackbarPos("Val Min","TrackBars")
     v_max = cv2.getTrackbarPos("Val Max","TrackBars")
-
-    print(h_min,h_max,s_min,s_max,v_min,v_max)
+    lower = np.array([h_min, s_min, v_min])
+    upper = np.array([h_max, s_max, v_max])
+    mask = cv2.inRange(imgHSV, lower, upper)
 
     cv2.imshow("Original", img)
     cv2.imshow("HSV", imgHSV)
+    cv2.imshow("Mask", mask)
     cv2.waitKey(1)
