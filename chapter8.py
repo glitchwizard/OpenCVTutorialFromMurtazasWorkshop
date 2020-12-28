cb5c83e1e2893669a868b2ce84862e8f5c72fe0b
charley mcgowan <charley.mcgowan@outlook.com>
2020-12-27 19:28:08 -0800

    images stacked together to see them more easily

--- chapter8.py
+++ chapter8.py
@@ -1,14 +1,15 @@
 import cv2
 import numpy as anp
+from chapter6 import stackImages
 
 path = 'Resources/shapes.png'
 img = cv2.imread(path)
 
 imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-imgBlur = cv2.GaussianBlur(imgGray, (7,7), 1)
+imgBlur = cv2.GaussianBlur(imgGray, (7, 7), 1)
 
-cv2.imshow("Original", img)
-cv2.imshow("Gray", imgGray)
-cv2.imshow("Blue", imgBlur)
+imgStack = stackImages(0.6, ([img, imgGray, imgBlur]))
+
+cv2.imshow("Stack", imgStack)
 cv2.waitKey(0)
 
