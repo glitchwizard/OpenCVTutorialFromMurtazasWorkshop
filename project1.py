fe2c81b19317e119ba0b5dc50589ddd2f259a192
charley mcgowan <charley.mcgowan@outlook.com>
2020-12-27 20:31:11 -0800

    Three different colors showing up now, with names

--- project1.py
+++ project1.py
@@ -6,23 +6,24 @@ cap.set(3, 640)
 cap.set(4, 480)
 
 myColors = [
-                [5, 107, 0, 19, 255, 255], #orange
-                [133, 56, 0, 159, 156, 255], #purple
-                [57, 76, 0, 100, 255, 255]  #green
+                [5, 107, 0, 19, 255, 255, "Orange"], #orange
+                [133, 56, 0, 159, 156, 255, "Purple"], #purple
+                [57, 76, 0, 100, 255, 255, "Green"]  #green
             ]
 
 
 def findColor(img, myColors):
     imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-    # lower = np.array([h_min, s_min, v_min])
-    # upper = np.array([h_max, s_max, v_max])
+    for color in myColors:
+        # lower = np.array([h_min, s_min, v_min])
+        # upper = np.array([h_max, s_max, v_max])
 
-    lower = np.array(myColors[0][0:3])
-    upper = np.array(myColors[0][3:6])
+        lower = np.array(color[0:3])
+        upper = np.array(color[3:6])
 
-    mask = cv2.inRange(imgHSV, lower, upper)
-    cv2.imshow("Mask Image", mask)
+        mask = cv2.inRange(imgHSV, lower, upper)
+        cv2.imshow(str(color[6]), mask)
 
 while True:
     success, img = cap.read()
