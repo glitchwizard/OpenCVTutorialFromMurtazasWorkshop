d85732864ee2394351a4b53588eb82424433add4
charley mcgowan <charley.mcgowan@outlook.com>
2020-12-27 19:30:08 -0800

    imgCanny added, and image stack resized

--- chapter8.py
+++ chapter8.py
@@ -1,5 +1,5 @@
 import cv2
-import numpy as anp
+import numpy as np
 from chapter6 import stackImages
 
 path = 'Resources/shapes.png'
@@ -7,8 +7,11 @@ img = cv2.imread(path)
 
 imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 imgBlur = cv2.GaussianBlur(imgGray, (7, 7), 1)
+imgCanny = cv2.Canny(imgBlur, 50, 50)
 
-imgStack = stackImages(0.6, ([img, imgGray, imgBlur]))
+imgBlank = np.zeros_like(img)
+imgStack = stackImages(0.6, ([img, imgGray, imgBlur],
+                             [imgCanny, imgBlank, imgBlank]))
 
 cv2.imshow("Stack", imgStack)
 cv2.waitKey(0)
