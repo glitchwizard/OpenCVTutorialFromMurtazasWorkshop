861374f81ccb16bbf7c4f3e2f2f8386d2ac3605e
charley mcgowan <charley.mcgowan@outlook.com>
2020-12-27 19:38:35 -0800

    contours successfully showing

--- chapter8.py
+++ chapter8.py
@@ -2,16 +2,27 @@ import cv2
 import numpy as np
 from chapter6 import stackImages
 
+def getContours(img):
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        print(area)
+
+        cv2.drawContours(imgContour, contour, -1, [255,0,0], 4)
+
 path = 'Resources/shapes.png'
 img = cv2.imread(path)
+imgContour = img.copy()
 
 imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 imgBlur = cv2.GaussianBlur(imgGray, (7, 7), 1)
 imgCanny = cv2.Canny(imgBlur, 50, 50)
 
+getContours(imgCanny)
+
 imgBlank = np.zeros_like(img)
 imgStack = stackImages(0.6, ([img, imgGray, imgBlur],
-                             [imgCanny, imgBlank, imgBlank]))
+                             [imgCanny, imgContour, imgBlank]))
 
 cv2.imshow("Stack", imgStack)
 cv2.waitKey(0)
