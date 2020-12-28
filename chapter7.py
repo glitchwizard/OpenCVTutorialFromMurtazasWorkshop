4939e2f5eb24638b3b08abefc26db975aeee0664
charley mcgowan <charley.mcgowan@outlook.com>
2020-12-27 18:57:22 -0800

    validated values on trackbars are being read correctly

--- chapter7.py
+++ chapter7.py
@@ -13,10 +13,19 @@ cv2.createTrackbar("Sat Max", "TrackBars", 255, 255, empty)
 cv2.createTrackbar("Val Min", "TrackBars", 0, 255, empty)
 cv2.createTrackbar("Val Max", "TrackBars", 255, 255, empty)
 
-img = cv2.imread(path)
+while True:
+    img = cv2.imread(path)
 
-imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    h_min = cv2.getTrackbarPos("Hue Min","TrackBars")
+    h_max = cv2.getTrackbarPos("Hue Max","TrackBars")
+    s_min = cv2.getTrackbarPos("Sat Min","TrackBars")
+    s_max = cv2.getTrackbarPos("Sat Max","TrackBars")
+    v_min = cv2.getTrackbarPos("Val Min","TrackBars")
+    v_max = cv2.getTrackbarPos("Val Max","TrackBars")
 
-cv2.imshow("Original", img)
-cv2.imshow("HSV", imgHSV)
-cv2.waitKey(0)
+    print(h_min,h_max,s_min,s_max,v_min,v_max)
+
+    cv2.imshow("Original", img)
+    cv2.imshow("HSV", imgHSV)
+    cv2.waitKey(1)
