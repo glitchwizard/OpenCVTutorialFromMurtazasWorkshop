72a081c0509ef69e157f7a0e185ef24ec3f5cd51
charley mcgowan <charley.mcgowan@outlook.com>
2020-12-27 18:11:22 -0800

    img display types complete.

--- chapter6.py
+++ chapter6.py
@@ -2,6 +2,10 @@ import cv2
 import numpy as np
 img = cv2.imread("Resources\kam.jpg")
 
+imgHor = np.hstack((img, img))
+imgVert = np.vstack((img,img))
 
+cv2.imshow("Horizontal", imgHor)
+cv2.imshow("Vertical", imgVert)
 
-cv2.waitkey(0)
+cv2.waitKey(0)
