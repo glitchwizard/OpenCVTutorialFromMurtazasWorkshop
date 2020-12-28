9dde3029b510211e39b05f678fdc72716575dd12
charley mcgowan <charley.mcgowan@outlook.com>
2020-12-27 17:45:09 -0800

    Add text to image

--- chapter4.py
+++ chapter4.py
@@ -9,6 +9,7 @@ img = np.zeros((512, 512, 3), np.uint8)
 cv2.line(img, (0, 0), (img.shape[1],img.shape[0]), (0, 255, 0),1)
 cv2.rectangle(img, (0, 0), (250, 350), (0, 0, 255), 1)
 cv2.circle(img, (400, 50), 30, (255, 255, 0), 2)
+cv2.putText(img, " OpenCV ", (300,100), cv2.FONT_HERSHEY_COMPLEX, 1 , (0,150,0,1))
 
 cv2.imshow("Image", img)
 
