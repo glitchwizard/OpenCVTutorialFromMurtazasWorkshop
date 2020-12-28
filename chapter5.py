6df25203d797c011d8d0e0c43ebcb0895d31f4f1
charley mcgowan <charley.mcgowan@outlook.com>
2020-12-27 18:02:11 -0800

    Card perspective working correctly

--- chapter5.py
+++ chapter5.py
@@ -3,8 +3,15 @@ import numpy as np
 
 img = cv2.imread("Resources\cards.jpg")
 
-pts1 = np.float32([[111,219],[287,188],[154,482],[352,440]])
+width, height = 300, 450
+pts1 = np.float32([[778, 268], [1162, 466], [509, 803], [894, 1001]])
+pts2 = np.float32([[0, 0], [width, 0], [0, height], [width, height]])
+
+matrix = cv2.getPerspectiveTransform(pts1,pts2)
+imgOutput = cv2.warpPerspective(img, matrix, (width, height))
+
 
 cv2.imshow("Image", img)
+cv2.imshow("Output", imgOutput)
 
 cv2.waitKey(0)
