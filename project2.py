c158c74684ea7efc0dfdb3a3f9f5398297092973
charley mcgowan <charley.mcgowan@outlook.com>
2020-12-28 16:46:26 -0800

    Preprocessing added

--- project2.py
+++ project2.py
@@ -1,17 +1,32 @@
 import cv2
+import numpy as np
 
 #Document Scanner using OpenCV
 
-frameWidth = 640
-frameHeight = 480
+widthImg = 640
+heightImg = 480
+
 captureDevice = cv2.VideoCapture(0)
-captureDevice.set(3, frameWidth)
-captureDevice.set(4, frameHeight)
-captureDevice.set(10, 130)
+captureDevice.set(3, widthImg)
+captureDevice.set(4, heightImg)
+captureDevice.set(10, 140)
+
+def preProcessing(img):
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 1)
+    imgCanny = cv2.Canny(imgBlur, 200, 200)
+    kernel = np.ones((5, 5))
+    imgDialation = cv2.dilate(imgCanny, kernel, iterations= 2)
+    imgThreshold = cv2.erode(imgDialation, kernel, iterations=1)
+
+    return imgThreshold
 
 while True:
     success, img = captureDevice.read()
-    cm2.imshow("Result", img)
+    img = cv2.resize(img, (widthImg, heightImg))
+    imgThreshold = preProcessing(img)
+    cv2.imshow("PreProcessed", imgThreshold)
+    cv2.imshow("Result", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
